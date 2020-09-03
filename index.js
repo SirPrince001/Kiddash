2b4267fd6dfa55772a09a54ccb44b3635c48fa65
Prince Bassey <basseyprince75@gmail.com>
2020-09-03 15:29:44 +0100

    "updated index file with host"

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = require('./app')
 require("dotenv").config();
 
-//const port = process.env.PORT || 3000;
-app.listen(process.env.PORT || 3000, () => {
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || '0.0.0.0'
+app.listen(port, host, () => {
   console.log("Listening to HTTP Server at port 8080");
 });
 
