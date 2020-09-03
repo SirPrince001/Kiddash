cededf0f9b645defe1292d9fc9ec9d8a91a29ac4
Prince Bassey <basseyprince75@gmail.com>
2020-09-03 14:22:54 +0100

    "updated with hosting port"

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = require('./app')
 require("dotenv").config();
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+//const port = process.env.PORT || 3000;
+app.listen(process.env.PORT || 3000, () => {
   console.log("Listening to HTTP Server at port 8080");
 });
 
